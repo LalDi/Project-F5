eab101c0dc68d3c97359e6f4ba5a3446ba140ff4
unis <uni423@naver.com>
2020-11-25 07:41:33 +0900

    Ver 0.0 (Beta 0.3)
    [Ingame]
    - 폴더 UI 배치 완료

---
+++ Stage_OnAir/Assets/02_Ingame/Scripts/SettingMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class SettingMenu : MonoBehaviour
+{
+    public bool IsOpen = false;
+    public GameObject[] Icons = new GameObject[3];
+    void Start()
+    {
+        for (int i = 0; i < Icons.Length; i++) {
+            Icons[i] = transform.GetChild(i).gameObject;
+            Icons[i].GetComponent<Button>().enabled = false;
+        }
+    }
+
+    public void Anim()
+    {
+        if (IsOpen) CloseAnim();
+        else OpenAnim();
+
+        IsOpen = !IsOpen;
+    }
+
+    void OpenAnim()
+    {
+        for (int i = 0; i < Icons.Length; i++)
+        {
+            Icons[i].transform.DOMoveY(transform.position.x + 300 * (i-1), 0.5f).SetEase(Ease.OutBack);
+            Icons[i].GetComponent<Image>().DOFade(1, 0.5f);
+            Icons[i].GetComponent<Button>().enabled = true;
+        }
+    }
+    void CloseAnim()
+    {
+        for (int i = 0; i < Icons.Length; i++)
+        {
+            Icons[i].transform.DOMoveY(1300, 0.5f).SetEase(Ease.OutBack);
+            Icons[i].GetComponent<Image>().DOFade(0, 0.2f);
+            Icons[i].GetComponent<Button>().enabled = false;
+        }
+    }
+}
