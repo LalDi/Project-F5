77ca250ba27cf7fb8a162558e4040337334f3cac
LalDi <jungjh0513@naver.com>
2020-11-24 20:05:47 +0900

    Ver 0.0 (Beta 0.2)
    [Game]
    - Illust씬 생성
    - Load씬 기능 추가
    (리소스 추가 X)
    
    [Ingame]
    - 매니저 스크립트 일부 작성
    - 불필요한 팝업창 제거
    - 부족한 팝업창 추가

--- Stage_OnAir/Assets/02_Ingame/Scripts/Manager/GameManager.cs
+++ Stage_OnAir/Assets/02_Ingame/Scripts/Manager/GameManager.cs
@@ -2,17 +2,75 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    // 연극의 3가지 점수 요소
+    public float Play_Quality { get; private set; }
+    public float Play_Marketing { get; private set; }
+    public float Play_Success { get; private set; }
+
+    // 연극의 퀄리티 점수를 결정하는 3가지 수치
+    public float Quality_Acting { get; private set; }
+    public float Quality_Scenario { get; private set; }
+    public float Quality_Direction { get; private set; }
+
+    // 현재 보유 금액
+    public int Money { get; private set; }
+
+    // 현재 날짜
+    public int Year { get; private set; }
+    public int Month { get; private set; }
+
+    // 설정된 연극 준비 기간
+    public int Period { get; private set; }
+
+    // 배우의 수
+    public int NowActor { get; private set; }
+    public int MaxActor { get; private set; }
+
+    //public List<ActorData.Actor> Actors;
+    //public Dictionary<string, StaffData> Staffs = new Dictionary<string, StaffData>();
+
+    public enum Step { Select_Scenario, Cast_Actor, Set_Period, Prepare_Play, Start_Play };
+    public Step NowStep;
+
+
+
+    private void Awake()
     {
-        
+        Screen.SetResolution(Screen.width, Screen.width * 9 / 16, true);
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        Application.targetFrameRate = 144;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Reset()
     {
-        
+        Play_Quality = 0;
+        Play_Marketing = 0;
+        Play_Success = 0;
+
+        Quality_Acting = 0;
+        Quality_Scenario = 0;
+        Quality_Direction = 0;
+
+        Period = 0;
+
+        NowActor = 0;
+        MaxActor = 0;
+
+        //Actors.Clear();
+        NowStep = Step.Select_Scenario;
     }
+
+    public void GoNextMonth()
+    {
+        Month++;
+        if (Month == 13)
+        {
+            Year++;
+            Month = 1;
+            // 스태프 연봉 지급하는 코드
+        }
+    }
+
 }
---
+++ Stage_OnAir/Assets/02_Ingame/Scripts/UIManager_02.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager_02 : MonoBehaviour
+{
+    [Header("Top UI")]
+    public Text Money;
+    public Text Year;
+    public Text Month;
+
+    [Header("Popup UI")]
+    public GameObject Popup_Black;
+    [Space(10)]
+    public GameObject Popup_Option;
+    public GameObject Popup_Rank;
+    [Space(10)]
+    public GameObject Popup_Audition;
+    public GameObject Popup_Period;
+    public GameObject Popup_Prepare;
+    public GameObject Popup_Marketing;
+    public GameObject Popup_MarketingCk;
+    public GameObject Popup_Develop;
+    public GameObject Popup_DevelopUp;
+    public GameObject Popup_DevelopCk;
+    public GameObject Popup_Play;
+    [Space(10)]
+    public GameObject Popup_Staff;
+    public GameObject Popup_StaffUp;
+    public GameObject Popup_Shop;
+
+}
