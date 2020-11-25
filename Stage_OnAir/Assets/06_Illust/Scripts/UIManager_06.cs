f7c73e64c9db3eb60a3c8b38b7cd8300c85d48e9
unis <uni423@naver.com>
2020-11-25 14:57:54 +0900

    Ver 0.0 (Beta 1.2)
    [Illust]
    - 일러스트 목록 UI 완료
    - 일러스트 버튼 누를 시 일러스트 이미지 활성화
    - 나가기 버튼 누를 시 일러스트 이미지 비활성화

---
+++ Stage_OnAir/Assets/06_Illust/Scripts/UIManager_06.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UIManager_06 : MonoBehaviour
+{
+    public GameObject Popup_Illust;
+
+    void Start()
+    {
+        Popup_Illust.SetActive(false);
+    }
+
+    public void Push_IllustBT()
+    {
+        Popup_Illust.GetComponent<Image>().sprite =
+            EventSystem.current.currentSelectedGameObject.transform.GetChild(1).GetComponent<Image>().sprite;
+        Popup_Illust.SetActive(true);
+    }
+    public void Exit_IllustPopup()
+    {
+        Popup_Illust.SetActive(false);
+    }
+}
