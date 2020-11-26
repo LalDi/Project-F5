52c80dd21c21138a5f51869b7ae79612ca173d69
unis <uni423@naver.com>
2020-11-26 14:28:27 +0900

    Title Var 0.1
    [Title]
    - 인게임 연결 기능 구현
    - Text 연출 효과 추가
    - 로그인 Popup UI 추가
    - 회원가임 Popup UI 추가

--- Stage_OnAir/Assets/02_Ingame/Scripts/Manager/GameManager.cs
+++ Stage_OnAir/Assets/02_Ingame/Scripts/Manager/GameManager.cs
@@ -38,7 +38,7 @@ public class GameManager : Singleton<GameManager>
 
     private void Awake()
     {
-        Screen.SetResolution(Screen.width, Screen.width * 9 / 16, true);
+        Screen.SetResolution(Screen.width, Screen.width * 16 / 9, true);
         Screen.sleepTimeout = SleepTimeout.NeverSleep;
         Application.targetFrameRate = 144;
     }
