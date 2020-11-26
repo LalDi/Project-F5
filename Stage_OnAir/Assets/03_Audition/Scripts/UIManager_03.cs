c008bef5c66b5e93762aa88b44690bd84782fe20
unis <uni423@naver.com>
2020-11-26 20:02:06 +0900

    TItle Var 0.1.1
    Save

---
+++ Stage_OnAir/Assets/03_Audition/Scripts/UIManager_03.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager_03 : MonoBehaviour
+{
+    public GameObject Popup_Balck;
+    public GameObject Popup_Result;
+
+    void Start()
+    {
+        Popup_Balck.SetActive(false);
+        Popup_Result.SetActive(false);
+    }
+    public void To_Ingame()
+    {
+        LoadManager.LoaderCallback();
+        LoadManager.Load(LoadManager.Scene.Ingame);
+    }
+}
