fc0fb04e66a047c72b349ebcbfcdfa162ea98974
unis <uni423@naver.com>
2021-03-09 23:57:12 +0900

    Ver 0.5
    [Ingame]
    - NPCManager 오브젝트 생성 NPC생성 및 이동 활성화

--- Stage_OnAir/Assets/02_Ingame/Scripts/NPCManager.cs
+++ Stage_OnAir/Assets/02_Ingame/Scripts/NPCManager.cs
@@ -1,72 +1,78 @@
-﻿//using Define;
-//using System.Collections;
-//using System.Collections.Generic;
-//using System.Linq;
-//using UnityEngine;
+﻿using Define;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
-//public class NPCManager : MonoBehaviour
-//{
-//    public GameObject Parent;
+public class NPCManager : MonoBehaviour
+{
+    public Transform Parent;
 
-//    public int[] Actors = new int[3]; //화면에 그려줄 배우 NPC 데이터
-//    public int[] Staffs = new int[3];; //화면에 그려줄 스탭 NPC 데이터
+    public int[] Actors = new int[3]; //화면에 그려줄 배우 NPC 데이터
+    public int[] Staffs = new int[3]; //화면에 그려줄 스탭 NPC 데이터
+    public int StaffCnt = 0;
 
-//    public GameObject[] Actor = new GameObject[8];
-//    public GameObject[] Staff = new GameObject[7];
+    public GameObject[] Actor = new GameObject[8];
+    public GameObject[] Staff = new GameObject[7];
 
-//    void Start()
-//    {
-//        Actors
-//        ActorCount += GameManager.Instance.NowActor;
+    void Start()
+    {
+        Actors = RandomActor(GameManager.Instance.NowActor);
 
-//        for(int i = 0; i < ActorCount; i++)
-//        {
-//            GameObject obj = GameManager.Instance.Staffs[Random.Range(0, )]
-//        }
-//    }
+        foreach (var item in GameManager.Instance.Staffs)
+            if (item.Level >= 1) StaffCnt++;
+        Staffs = RandomStaff(StaffCnt);
 
-//    void Update()
-//    {
-        
-//    }
+        for (int i = 0; i < Actors.Count(); i++) {
+            GameObject obj = Instantiate(Actor[Actors[i]], Parent);
+            obj.transform.localPosition = new Vector3(-800 * (i - 1), -800);
+        }
+        for (int i = 0; i < Staffs.Count(); i++)
+        {
+            GameObject obj = Instantiate(Staff[Staffs[i]], Parent);
+            obj.transform.localPosition = new Vector3(-800 * (i - 1), -1100);
+        }
+    }
 
-//    public int[] RandomActor(int count)
-//    {
-//        int[] RandomActor = new int[];
+    public int[] RandomActor(int count)
+    {
+        if (count > 3) count = 3;
 
-//        List<Actor> Select = new List<Actor>();
+        int[] RandomActor = new int[count];
+        List<Actor> Select = new List<Actor>();
 
-//        Select = GameManager.Instance.Actors;
-//        Select = Math.ShuffleList(Select);
+        Select = GameManager.Instance.Actors;
+        Select = Math.ShuffleList(Select);
 
-//        for (int i = 0; i < count; i++)
-//        {
-//            RandomActor[i] = Select[i].No;
-//        }
+        for (int i = 0; i < count; i++)
+        {
+            RandomActor[i] = Select[i].No;
+        }
 
-//        return RandomActor;
-//    }
-//    public int[] RandomStaff(int count)
-//    {
-//        int[] RandomStaff = new int[];
+        return RandomActor;
+    }
+    public int[] RandomStaff(int count)
+    {
+        if (count > 3) count = 3;
 
-//        List<Actor> Select = new List<Actor>();
+        int[] RandomStaff = new int[count];
+        List<Staff> Select = new List<Staff>();
 
-//        int value = 0;
+        int value = 0;
 
-//        foreach(var item in GameManager.Instance.Staffs)
-//        {
-//            if (item.Level >= 1)
-//                Select[value] = item.n
-//        }
+        foreach (var item in GameManager.Instance.Staffs)
+        {
+            if (item.Level >= 1)
+                Select.Add(item);
+        }
 
-//        Select = Math.ShuffleList(Select);
+        Select = Math.ShuffleList(Select);
 
-//        for (int i = 0; i < count; i++)
-//        {
-//            RandomStaff[i] = Select[i].No;
-//        }
+        for (int i = 0; i < count; i++)
+        {
+            RandomStaff[i] = Select[i].Code;
+        }
 
-//        return RandomStaff;
-//    }
-//}
+        return RandomStaff;
+    }
+}
