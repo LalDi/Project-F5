e5ef5ec290d29c28bd6fca48fca30a2175b7dc33
LalDi <jungjh0513@naver.com>
2021-04-26 19:32:23 +0900

    Google ver 1.0
    [Unity]
     - Google Play Game Plugin 추가
     - 서명 키 적용
    
    [Title]
     - 테스트용 구글 로그인 코드 적용
    (실행 안 됨)

---
+++ Stage_OnAir/Assets/01_Title/Scripts/GoogleTestLogin.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using BackEnd;
+
+public class GoogleTestLogin : MonoBehaviour
+{
+    // GPGS 로그인 
+    void Start()
+    {
+        // GPGS 플러그인 설정
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration
+            .Builder()
+            .RequestServerAuthCode(false)
+            .RequestEmail() // 이메일 권한을 얻고 싶지 않다면 해당 줄(RequestEmail)을 지워주세요.
+            .RequestIdToken()
+            .Build();
+        //커스텀된 정보로 GPGS 초기화
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.DebugLogEnabled = true; // 디버그 로그를 보고싶지 않다면 false로 바꿔주세요.
+                                                  //GPGS 시작.
+        PlayGamesPlatform.Activate();
+    }
+
+    public void GPGSLogin()
+    {
+        // 이미 로그인 된 경우
+        if (Social.localUser.authenticated == true)
+        {
+            BackendReturnObject BRO = Backend.BMember.AuthorizeFederation(GetTokens(), FederationType.Google, "gpgs");
+        }
+        else
+        {
+            Social.localUser.Authenticate((bool success) => {
+                if (success)
+                {
+                    // 로그인 성공 -> 뒤끝 서버에 획득한 구글 토큰으로 가입요청
+                    BackendReturnObject BRO = Backend.BMember.AuthorizeFederation(GetTokens(), FederationType.Google, "gpgs");
+                }
+                else
+                {
+                    // 로그인 실패
+                    Debug.Log("Login failed for some reason");
+                }
+            });
+        }
+    }
+
+    // 구글 토큰 받아옴
+    public string GetTokens()
+    {
+        if (PlayGamesPlatform.Instance.localUser.authenticated)
+        {
+            // 유저 토큰 받기 첫번째 방법
+            string _IDtoken = PlayGamesPlatform.Instance.GetIdToken();
+            // 두번째 방법
+            // string _IDtoken = ((PlayGamesLocalUser)Social.localUser).GetIdToken();
+            return _IDtoken;
+        }
+        else
+        {
+            Debug.Log("접속되어있지 않습니다. PlayGamesPlatform.Instance.localUser.authenticated :  fail");
+            return null;
+        }
+    }
+
+    public void SetDataInit()
+    {
+        //Backend.BMember.RefreshTheBackendToken((callback) => { });
+
+        Backend.Chart.GetAllChartAndSave(true);
+        ScenarioData.Instance.SetScenarioData();
+        ActorData.Instance.SetActorsData();
+        MarketingData.Instance.SetMarketingData();
+        GameManager.Instance.Init();
+    }
+}
