0f80a3b2a3b7f4e4c6c3f2c2cf23c078ccc6e12b
LalDi <jungjh0513@naver.com>
2021-06-22 19:20:04 +0900

    Hotfix
    [Play]
    - 패키지 적용 중에는 일러스트를 획득할 수 없도록 수정

--- Stage_OnAir/Assets/05_Play/Scripts/UIManager_05.cs
+++ Stage_OnAir/Assets/05_Play/Scripts/UIManager_05.cs
@@ -161,9 +161,11 @@ public class UIManager_05 : MonoBehaviour
 
     public void Illust()
     {
-        if (GameManager.Instance.Play_Success >= 90 && Success) //일정 성공률 이상이면 일러스트 해금
+        if (!(GameManager.Instance.OnPackage == true && GameManager.Instance.UsePackage == true) && // 패키지 적용중이 아니면서
+            GameManager.Instance.Play_Success >= 90 && Success) //일정 성공률 이상이면 일러스트 해금
             GameManager.Instance.ScenarioIllust[GameManager.Instance.NowScenario.No - 1] = true;
-        else { 
+        else
+        {
             To_Ingame();
             return;
         }
